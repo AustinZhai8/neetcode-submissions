247ad5f09adc6d3d234afbdcf295d6947cc307ca
Austin Zhai <austinhzhai@gmail.com>
2026-05-05 21:33:45 -0600

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        countS = {}
+        countT = {}
+
+        for i in range(len(s)):
+            if s[i] in countS:
+                countS[s[i]] += 1
+            else:
+                countS[s[i]] = 1
+
+        for i in range(len(t)):
+            if t[i] in countT:
+                countT[t[i]] += 1
+            else:
+                countT[t[i]] = 1
+        
+        if (countS == countT):
+            return True
+        
+        else:
+            return False
